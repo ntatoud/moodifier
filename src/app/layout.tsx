992e77a81bddfd4f466deb9d8fe26c0ad889cdd1
Noe TATOUD <noe.tatoud@gmail.com>
2024-05-10 21:41:57 +0200

    feat(setup): Init with t3app and shadcn

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import "@/styles/globals.css";
+
+import { Poppins, Quicksand } from "next/font/google";
+
+import { TRPCReactProvider } from "@/trpc/react";
+import { cn } from "@/lib/utils";
+
+const headingFont = Poppins({
+  subsets: ["latin"],
+  variable: "--font-heading",
+  weight: ["700"],
+});
+
+const defaultFont = Quicksand({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export const metadata = {
+  title: "Moodifier",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={cn(headingFont.variable, defaultFont.variable)}>
+      <body className="flex min-h-screen flex-col">
+        <TRPCReactProvider>{children}</TRPCReactProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default async function Home() {
+  return (
+    <main className="flex flex-1 flex-col items-center justify-center">
+      <h1 className="font-heading text-4xl font-bold">
+        Welcome to your new project!
+      </h1>
+      <p className="mt-4 font-sans text-lg">This is gonna be awesome!</p>
+    </main>
+  );
+}
